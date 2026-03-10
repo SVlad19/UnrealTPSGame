ead0156782f2b278a74339026fd97df12c66aa17
SVlad19 <mr.vladik.semeno3@gmail.com>
2026-03-10 11:15:44 +0200

    Initial commit

---
+++ Build.bat
@@ -0,0 +1,6 @@
+"E:\EpicGames\UE_5.4\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project=E:\UE_Projects\TPS\TPS.uproject ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory=E:\UE_Projects\TPS\BuildFromBat ^
+-build -cook -stage -archive -pak -package -target=TPS 
---
+++ Source/TPS/TPS.Build.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System;
+
+public class TPS : ModuleRules
+{
+	public TPS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Console.WriteLine("TPS module rules ------------------------------>");
+		Console.WriteLine("Unreal Version: {0}.{1}.{2}", Target.Version.MajorVersion, Target.Version.MinorVersion, Target.Version.PatchVersion);
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
